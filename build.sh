7c6e9ee91c78108a4ce99a8227969649da64b1a4
Miro Kropacek <miro.kropacek@gmail.com>
2016-12-25 15:33:18 +1000

    Merge pull request #8 from miniupnp:select_target. This resolves #4.

--- build.sh
+++ build.sh
@@ -5,7 +5,42 @@ CPU_DIRS=("m68000"	"m68020-60"	"m5475")	# target directory
 CPU_OPTS=("m68000"	"m68020-60"	"mcpu=5475")	# gcc command line
 CPU_CPUS=("m68000"	"m68020-60"	"5475")		# --with-cpu=
 
-indices=$(seq 0 $((${#CPU_DIRS[@]} - 1)))
+indices=""
+indices_all=$(seq 0 $((${#CPU_DIRS[@]} - 1)))
+skip_native=0
+
+# parse command line
+for arg in $*
+do
+	case $arg in
+	--skip-native)
+	skip_native=1
+	;;
+	--all)
+	indices=$indices_all
+	;;
+	*)
+	ok=0
+	for i in $(seq 0 $((${#CPU_DIRS[@]} - 1))) ; do
+		if [ "$arg" == "${CPU_CPUS[i]}" ] ; then
+			indices="$indices $i"
+			ok=1
+		fi
+	done
+	if [ $ok -eq 0 ] ; then
+		echo "Error : $arg is not a valid CPU."
+		echo "Valid CPU are : ${CPU_CPUS[*]}"
+		exit 1
+	fi
+	;;
+	esac
+done
+
+if [ -z "$indices" ] ; then
+	echo "No CPU to build."
+	exit 1
+fi
+
 
 for i in $indices
 do
@@ -17,7 +52,9 @@ do
 	${MAKE} gcc-multilib-patch OPTS="$multilib_opts" DIRS="$multilib_dirs" || exit 1
 	
 	${MAKE} binutils gcc-preliminary INSTALL_DIR="$INSTALL_DIR/$dir" CPU="$cpu" || exit 1
-	for j in $indices
+
+	# build mintlib and pml for all targets
+	for j in $indices_all
 	do
 		target="${CPU_DIRS[j]}"
 		prefix="$INSTALL_DIR/$dir/m68k-atari-mint"
@@ -30,21 +67,28 @@ do
 	done
 	${MAKE} gcc mintbin INSTALL_DIR="$INSTALL_DIR/$dir" CPU="$cpu" || exit 1
 	
-	${MAKE} binutils-atari INSTALL_DIR="$INSTALL_DIR/$dir" CPU="$cpu" || exit 1
-	if [ "$cpu" == "5475" ]
-	then
-		assembly="--disable-assembly"
-	else
-		assembly=""
+	if [ $skip_native -eq 0 ] ; then
+		${MAKE} binutils-atari INSTALL_DIR="$INSTALL_DIR/$dir" CPU="$cpu" || exit 1
+		if [ "$cpu" == "5475" ]
+		then
+			assembly="--disable-assembly"
+		else
+			assembly=""
+		fi
+		${MAKE} gcc-atari ASSEMBLY="$assembly" INSTALL_DIR="$INSTALL_DIR/$dir" CPU="$cpu" || exit 1
 	fi
-	${MAKE} gcc-atari ASSEMBLY="$assembly" INSTALL_DIR="$INSTALL_DIR/$dir" CPU="$cpu" || exit 1
 	
 done
 
-${MAKE} strip-atari INSTALL_DIR="$INSTALL_DIR/${CPU_DIRS[0]}"	# use either 'strip'
+if [ $skip_native -eq 0 ] ; then
+	# use either 'strip'
+	i=$(echo $indices | cut -d " " -f 1)
+	${MAKE} strip-atari INSTALL_DIR="$INSTALL_DIR/${CPU_DIRS[i]}"
 
-for i in $indices
-do
-	cpu="${CPU_CPUS[i]}"
-	${MAKE} pack-atari INSTALL_DIR="$INSTALL_DIR/$dir" CPU="$cpu" || exit 1
-done
+	for i in $indices
+	do
+		cpu="${CPU_CPUS[i]}"
+		${MAKE} pack-atari INSTALL_DIR="$INSTALL_DIR/$dir" CPU="$cpu" || exit 1
+	done
+
+fi
